5caa33567174c3681327d4090f9964433a9b0918
Julian Garzon <jdjuliangs@gmail.com>
2022-11-16 13:25:35 -0500

    Controller Advice

---
+++ src/main/java/com/jg/exceptions/GlobalHandlerException.java
@@ -0,0 +1,15 @@
+package com.jg.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalHandlerException {
+
+    @ExceptionHandler({CustomNotFoundException.class, FileNotFoundException.class, CatalogoException.class})
+    public ResponseEntity<String> handleException(RuntimeException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
