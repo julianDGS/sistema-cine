7b0d71d1099db7036cf3aeda972412cc2364dcd3
Julian Garzon <jdjuliangs@gmail.com>
2022-11-16 17:48:21 -0500

    Ajustes Entidad Usuario

--- src/main/java/com/jg/security/domain/Rol.java
+++ src/main/java/com/jg/security/domain/Rol.java
@@ -1,47 +1,21 @@
 package com.jg.security.domain;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Data
 @Table(name = "rol")
-public class Rol implements Serializable {
-    private static final long serialVersionUID = 1L;
-    
+public class Rol  {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idRol;
     
     @NotNull
     private String nombre;
-    
-    public Rol(Long idRol) {
-        this.idRol = idRol;
-    }
-
-    public Rol() {
-    }
-
-    public Rol(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Long getIdRol() {
-        return idRol;
-    }
-
-    public void setIdRol(Long idRol) {
-        this.idRol = idRol;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
     
 }
--- src/main/java/com/jg/security/domain/Usuario.java
+++ src/main/java/com/jg/security/domain/Usuario.java
@@ -1,5 +1,8 @@
 package com.jg.security.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,10 +10,10 @@ import javax.persistence.*;
 import javax.validation.constraints.*;
 
 @Entity
+@Data
 @Table(name = "usuario")
-public class Usuario implements Serializable {
-    
-    private static final long serialVersionUID = 1L;
+@NoArgsConstructor
+public class Usuario {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,54 +39,9 @@ public class Usuario implements Serializable {
     )
     private Set<Rol> roles = new HashSet<>();
 
-    public Usuario() {
-    }
-
     public Usuario(String username, String password, String email) {
         this.username = username;
         this.password = password;
         this.email = email;
     }
-
-    public Long getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(Long idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Rol> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Rol> roles) {
-        this.roles = roles;
-    }
-    
-    
 }
